6248182c92754b00e2cb47e25d112e2d069e16aa
Jeya Antony <jchithra@healthgrades.com>
2019-05-01 17:15:59 +0530

    Added Profile component changes

--- src/app/health-points/profile/profile.component.ts
+++ src/app/health-points/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileService } from 'src/app/services/profile.service';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  profileName: string = '';
+  userName: string = '';
+  imageUrl: string = '';
+  firstName: string = '';
+  lastName: string = '';
 
-  ngOnInit() {
+  constructor(private profileService: ProfileService,
+    private dataService: DataService) {
   }
 
+  ngOnInit() {
+    this.profileName = this.dataService.currentUser.FIRST_NAME + " " +this.dataService.currentUser.LAST_NAME;
+    console.log(this.dataService.currentUser);
+    //this.imageUrl = this.dataService.currentUser.AVATAR;
+    this.firstName = this.dataService.currentUser.FIRST_NAME;
+    this.lastName = this.dataService.currentUser.LAST_NAME;
+    this.userName = this.dataService.currentUser.USERNAME;
+  }
 }
